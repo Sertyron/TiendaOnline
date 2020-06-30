e70bfe0033349f22619ab81198d1fc8a2a929c0c
Alejandro Garcia <50833238+Sertyron@users.noreply.github.com>
2020-06-30 13:14:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+/* let baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Papa',
+        precio: 55,
+        imagen: '../src/papa.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'Cebolla',
+        precio: 20,
+        imagen: '../src/cebolla.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'Calabacin',
+        precio: 85,
+        imagen: '../src/calabacin.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'Frutillas',
+        precio: 60,
+        imagen: '../src/frutilla.jpg'
+    },
+    {
+        id: 5,
+        nombre: 'Frutillas',
+        precio: 96,
+        imagen: '../src/frutilla.jpg'
+    },
+    {
+        id: 6,
+        nombre: 'Frutillas Grandes',
+        precio: 116,
+        imagen: '../src/frutilla.jpg'
+    }
+
+
+]
+
+console.log(baseDeDatos[0]); */
